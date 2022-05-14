8e1341001475953d0d74d7c10e873cd73d406a8b
sinkaroid <bibsindra@gmail.com>
2022-05-15 04:53:29 +0700

    feat: add base constant

---
+++ src/const.ts
@@ -0,0 +1,29 @@
+export default {
+  endpoint: {
+    baseurl: "https://discord.com/api/v9",
+    hello: ["Wêreld!", "Botë!", "ልዑል!", "بالعالم!", "աշխարհ!", "Mundua!", "Сусвет!", "বিশ্ব!", "свят!", "món!", "Lapansi!", "你好世界！", "svijete!", "světe!", "Verden!", "Wereld!", "World!", "maailm!", "maailma!", "monde!", "wrâld!", "მსოფლიო!", "Welt!", "Κόσμε!", "Duniya!", "עולם!", "दुनिया!", "Világ!", "heimur!", "Ụwa!", "Dunia!", "mondo!", "こんにちは世界！", "Әлем!", "សួស្តី​ពិភពលោក!", "дүйнө!", "ສະ​ບາຍ​ດີ​ຊາວ​ໂລກ!", "pasaule!", "pasauli!", "Welt!", "свету!", "dunia!", "വേൾഡ്!", "дэлхий!", "မင်္ဂလာပါကမ္ဘာလောက!", "संसार!", "Verden!", "نړی!", "دنیا!", "świecie!", "Mundo!", "ਦੁਨਿਆ!", "Lume!", "мир!", "Shaoghail!", "Свете!", "Lumela!", "වර්ල්ඩ්!", "svet!", "Dunya!", "Dunia!", "världen!", "Ҷаҳон!", "สวัสดีชาวโลก!", "Dünya!", "Світ!", "Dunyo!", "giới!", "Byd!", "Lizwe!", "וועלט!", "Aiye!", "Mhlaba!"]
+  },
+  error: {
+    missingToken: "The authorization token is missing.",
+    invalidToken: "Invalid token.",
+    notValidCode: "Put code only.",
+    notArray: "The parameter must be an array.",
+    invalidDelay: "Interval must be at least 1000ms",
+  },
+  channelType: [
+    0, // GUILD_TEXT
+    1, // DM
+    2, // GUILD_VOICE
+    3, // GROUP_DM
+    4, // GUILD_CATEGORY
+    5, // GUILD_NEWS
+    10, // GUILD_NEWS_THREAD
+    11, // GUILD_PUBLIC_THREAD
+    12, // GUILD_PRIVATE_THREAD
+    13, // GUILD_STAGE_VOICE
+    14 // GUILD_DIRECTORY
+  ],
+  pending: {
+    limit: 1000
+  }
+};
